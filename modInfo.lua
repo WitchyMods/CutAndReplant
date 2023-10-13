1183768a3677399ace6314e870036823fb5962e0
Witchy <witchymods@gmail.com>
2023-10-13 03:07:37 -0400

    Add project files.

---
+++ modInfo.lua
@@ -0,0 +1,9 @@
+local modInfo = {
+  name = "Cut and Replant",
+  description = "Allows you to cut and replant trees in one action",
+  type = "world",
+  developer = "Witchy",
+  version = "1.0.0",
+  preview = "preview.png"
+}
+return modInfo
